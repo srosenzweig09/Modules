d5b14885486012f1e43ae4de37445008b0b40087
Suzanne Rosenzweig <srosenzweig09@gmail.com>
2021-06-30 13:30:09 -0400

    Added normalization function.

--- consistent_plots.py
+++ consistent_plots.py
@@ -26,3 +26,9 @@ def hist(ax, x, bins=100, label=None, weights=None, color=None, density=False):
 def hist2d(ax, x, y, xbins=100, ybins=100, norm=LogNorm()):
     cmap = change_cmap_bkg_to_white('rainbow')
     return ax.hist2d(x, y, bins=(xbins, ybins), norm=LogNorm(), cmap=cmap)
+
+def norm_hist(arr, bins=100):
+    n, b = np.histogram(arr, bins=bins)
+    x = (b[:-1] + b[1:]) / 2
+    
+    return n/n.max(), b, x
