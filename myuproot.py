c8a30b61947be2771d6ad9c69c7fbc1df923fa3b
Suzanne <srosenzweig09@gmail.com>
2021-08-05 16:50:19 -0400

    Working with filters.

--- myuproot.py
+++ myuproot.py
@@ -1,38 +1,27 @@
 ## Open ROOT files using uproot without having to go through the entire rigamaroll each time (load file, get branches, convert to arrays)
 
+import colorama
+colorama.init(autoreset=True)
+from colorama import Fore, Back
 import uproot
 import numpy as np
 from logger import info
 
-def open_up(fileName, treeName='sixBtree'): #, open_tree=True):
-    """
-    Opens a ROOT file using uproot and prints branch names.
+def unfiltered_tree(tree):
 
-    :param fileName: ROOT file to open
-    :param treeName: tree in ROOT file to open
-    :return: uprootTTree, awkwardArrayTree, NumPyDict
-    """
-    tree = uproot.open(fileName+':'+treeName)
-
-    # if open_tree:
-    #     tree = uproot.open(fileName+':'+treeName)
-    # else:
-    #     file = uproot.open(fileName)
-    #     tree = file[treeName]
-
-    table   = tree.arrays()
-    keys    = tree.keys()
-    n       = len(keys)
-    nevents = len(table)
-    nptab   = tree.arrays(library='np')
+    ak_dict = tree.arrays()
+    keys     = tree.keys()
+    n        = len(keys)
+    nevents  = len(table)
+    np_dict = tree.arrays(library='np')
 
     ncols = 3
     modu  = n%ncols
     
-    info(f"Tree contains {nevents} events.")
     print("-"*100)
     print(" "*44 + "TABLE COLUMNS" + " "*43)
     print("-"*100)
+    info(f"Tree contains {nevents} events.")
     
     for i in np.arange(0,n-modu,ncols):
         row = ""
@@ -46,4 +35,39 @@ def open_up(fileName, treeName='sixBtree'): #, open_tree=True):
         print(row)
     print("-"*100)
 
-    return tree, table, nptab
+    return ak_dict, np_dict
+
+def filtered_tree(tree, filter, dict_keys):
+
+    ak_dicts = {}
+    np_dicts = {}
+    for f,k in zip(filter, dict_keys):
+        ak_dicts[k] = tree.arrays(filter_name=f)
+        np_dicts[k] = tree.arrays(filter_name=f, library='np')
+
+    print(f"Passing ak and np dicts with keys {ak_dicts.keys()}.")
+
+    return ak_dicts, np_dicts
+
+def open_up(fileName, treeName='sixBtree', filter=None, dict_keys=None): #, open_tree=True):
+    """
+    Opens a ROOT file using uproot and prints branch names.
+
+    :param fileName: ROOT file to open
+    :param treeName: tree in ROOT file to open
+    :return: uprootTTree, awkwardArrayTree, NumPyDict
+    """
+
+    info(f"Opening ROOT file {fileName} with columns")
+    tree = uproot.open(fileName+':'+treeName)        
+
+    if filter:
+        assert dict_keys, print("ERROR: Please provide dict keys with filter arg.")
+        ak_dict, np_dict = filtered_tree(tree, filter, dict_keys)
+        return tree, ak_dict, np_dict
+    else:
+        ak_dict, np_dict = unfiltered_tree(tree)
+        return tree, ak_dict, np_dict
+
+
+
